5d8078a6144991297fce5c82dca26f11eef51a4d
Somya <Somya95singh@gmail.com>
2022-04-29 14:44:22 +0530

    first Git commit with java project

---
+++ src/Strings/Extract_the_integer.java
@@ -0,0 +1,29 @@
+package Strings;
+import java.util.Scanner;
+
+//1. Prakhar Aggrawal, 2. Manish Kumar Rai, 3. Rishabh Gupta56
+
+public class Extract_the_integer {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		String s = "";
+		System.out.println(str);
+		for(int i = 0; i<str.length()-1; i++){
+			if(str.charAt(i)>='0' && str.charAt(i)<='9'){
+				s += str.charAt(i);
+				if(str.charAt(i+1)>='0' && str.charAt(i+1)<='9') {
+					continue;
+				}
+				else{
+					s += " ";
+				}
+			}
+
+		}
+		if(str.charAt(str.length()-1)>='0' && str.charAt(str.length()-1)<='9')
+			s += str.charAt(str.length()-1);
+
+		System.out.println(s);
+	}
+}
