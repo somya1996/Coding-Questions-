de5f1b054926fec11eec91ec97642dce4f6cd1a3
Somya <Somya95singh@gmail.com>
2022-04-30 21:02:04 +0530

    first Git commit with java project

---
+++ src/Strings/Checkrotation.java
@@ -0,0 +1,44 @@
+package Strings;
+
+import java.util.Scanner;
+/*
+INPUT:                                OUTPUT
+geeksforgeeks                                TRUE
+forgeeksgeeks
+
+mightandmagic                                FALSE
+andmagicmigth
+*/
+public class Checkrotation {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s1 = sc.nextLine();
+		String s2 = sc.nextLine();
+		System.out.println(to_check(s1,s2));
+	}
+	public static boolean to_check(String s1 , String s2){
+		String s = "";
+		boolean flag = true;
+		if(s1.length() != s2.length()) {
+			return false;
+		}
+		/*else if(!s1.equals(s2))
+			flag =  false;*/
+		else if(s1.equals(s2))
+			flag = true;
+		else {
+			int l = s1.length();
+			for (int i = 1; i < s1.length(); i++) {
+				s += s1.charAt(i-1);
+				System.out.println(" s " + s);
+				System.out.println(" substring of s1 " + s1.substring(i, l));
+				String ans = s1.substring(i,l)+s;
+				System.out.println(" ans "+ans);
+				if (s2.equals(ans)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+}
