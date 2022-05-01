b22578e69878f17cbf2c0908e2229d1205c31183
Somya <Somya95singh@gmail.com>
2022-05-01 16:17:27 +0530

    first Git commit with java project

--- src/Strings/Make_Anagram.java
+++ src/Strings/Make_Anagram.java
@@ -1,8 +1,5 @@
 package Strings;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 public class Make_Anagram {
 	public static void main(String[] args) {
@@ -11,32 +8,40 @@ public class Make_Anagram {
 		String s2 = sc.nextLine();
 		int count = 0;
 
-		/*HashMap<Character, Integer> hm = new HashMap<Character, Integer>();
-		for(int i=0; i<s1.length();i++){
-			if(hm.containsKey(s1.charAt(i)))
+		HashMap<Character, Integer> hm = new HashMap<Character, Integer>();
+		for (int i = 0; i < s1.length(); i++) {
+			if (hm.containsKey(s1.charAt(i)))
 				hm.put(s1.charAt(i), hm.get(s1.charAt(i)) + 1);
 			else
-				hm.put(s1.charAt(i),1);
+				hm.put(s1.charAt(i), 1);
 		}
-		System.out.println(" first "+hm);
-		for(int i=0;i<s2.length();i++){
-			if(hm.containsKey(s2.charAt(i)))
+		System.out.println(" first " + hm);
+		for (int i = 0; i < s2.length(); i++) {
+			if (hm.containsKey(s2.charAt(i))) {
+
 				hm.put(s2.charAt(i), hm.get(s2.charAt(i)) - 1);
-			else
-				hm.put(s2.charAt(i),1);
+			}
+			else {
+				hm.put(s2.charAt(i), -1);
+			}
 		}
-		System.out.println(" second "+hm);*/
-		char c[] = new char[26];
-		for(int i=0;i<s1.length();i++){
-			c[s1.charAt(i)-'a']++;
+		System.out.println(" second " + hm);
+		/*for (Map.Entry<Character, Integer> entry : hm.entrySet()) {
+			if (entry.getValue()<0 || entry.getValue()==1)
+				count+= entry.getValue();
+		}*/
+		for(int i: hm.values())
+		{
+			if(i<0) {
+				count = count - i;
+				System.out.println(" i less : "+i+" "+count);
+			}
+			else {
+				count = count + i;
+				System.out.println(" i greater : "+i+" "+count);
+			}
 		}
-		for(int i=0;i<s2.length();i++){
-			c[s2.charAt(i)-'a']--;
-		}
-		for(int i=0;i<c.length;i++){
-			if(c[i]==1)
-				count++;
-		}
+
 		System.out.println(count);
 	}
 }
