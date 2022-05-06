ff43355bd2bbba5197c5e697c2024797ad43211e
Somya <Somya95singh@gmail.com>
2022-05-06 09:08:35 +0530

    first Git commit with java project

---
+++ src/Strings/Count_characters.java
@@ -0,0 +1,42 @@
+package Strings;
+/*
+Given a string S. Count the characters that have ‘N’ number of occurrences. If a character appears consecutively it is counted as 1 occurrence.
+
+Example 1:
+
+Input:
+S = "abc", N = 1
+Output: 3
+Explanation: 'a', 'b' and 'c' all have 1 occurrence.
+
+Input:
+S = "geeksforgeeks", N = 2
+Output: 4
+Explanation: 'g', 'e', 'k' and 's' have
+2 occurrences.
+*/
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Count_characters {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		int n = sc.nextInt();
+		int c[] = new int[256];
+		Arrays.fill(c,0);
+		for(int i=0; i<s.length(); i++){
+			c[s.charAt(i)-'0']++;
+		}
+		/*for(int i=0; i<c.length; i++){
+			System.out.print(c[i]+" ");
+		}*/
+		System.out.println();
+		int count = 0;
+		for(int i=0; i<c.length; i++){
+			if(c[i] == n)
+				count++;
+		}
+		System.out.println("count"+count);
+	}
+}
