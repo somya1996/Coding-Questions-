77b5a327d211eed04d75e76f937343dd30f8f7cf
Somya <Somya95singh@gmail.com>
2022-05-08 16:26:11 +0530

    first Git commit with java project

--- src/Array/medium_level/prefix_sum/By_removing_make_even_odd_sum.java
+++ src/Array/medium_level/prefix_sum/By_removing_make_even_odd_sum.java
@@ -16,28 +16,49 @@ public class By_removing_make_even_odd_sum {
 		evensum[0] = A[0];
 		oddsum[0] = 0;
 
-		for(int i=1; i<n; i++){
-			if(i%2==0)
+		for(int i=1; i<=A.length-1; i++){
+			if(i%2 == 0){
 				evensum[i] = evensum[i-1]+A[i];
-			else
-				evensum[i] = evensum[i-1];
-		}
-
-		for(int i=1; i<n; i++){
-			if(i%2!=0)
-				oddsum[i] = oddsum[i-1]+A[i];
-			else
 				oddsum[i] = oddsum[i-1];
+			}
+			else{
+				evensum[i] = evensum[i-1];
+				oddsum[i] = oddsum[i-1] + A[i];
+			}
 		}
 
+		//for(int i=0; i<n; i++){
+		//	System.out.println("evensum "+ evensum[i]);
+		//	System.out.println("oddsum "+ oddsum[i]);
+		//}
+		int result = 0;
 		for(int i=0; i<n; i++){
-			System.out.println("evensum "+ evensum[i]);
-			System.out.println("oddsum "+ oddsum[i]);
+			if(i==0) {
+				if (oddsum[A.length - 1] - oddsum[i] == evensum[A.length - 1] - evensum[i])
+					result++;
+			}
+
+			else if(i == n-1){
+				if(oddsum[A.length-2] == evensum[A.length-2])
+					result++;
+			}
+			else{
+				int odd = oddsum[i-1] + evensum[A.length-1] - evensum[i] ;
+				int even = evensum[i-1] + oddsum[A.length-1] - oddsum[i];
+				if(even == odd)
+					result++;
+			}
+			/*
+			System.out.println(" i "+i);
+				int even = evensum[i-1] + oddsum[A.length-1] - oddsum[i] ;
+				System.out.println("even "+even+ " evensum[i-1] "+ evensum[i-1]+" oddsum[A.length-1] "+oddsum[A.length-1] +" oddsum[i] "+oddsum[i]);
+				int odd = oddsum[i-1] + evensum[A.length-1] - evensum[i];
+				System.out.println("odd "+odd+ " oddsum[i-1] "+ oddsum[i-1]+" evensum[A.length-1] "+evensum[A.length-1] +" evensum[i] "+evensum[i]);
+				if(even == odd)
+					result++;
+			 */
+
 		}
-
-		for(int i=0; i<n; i++){
-
-
-		}
+		System.out.println(result);
 	}
 }
