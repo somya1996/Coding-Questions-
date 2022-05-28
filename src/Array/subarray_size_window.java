aa26b911595c4ffc3d70ad57b2aa4e65d6c32a47
Somya <Somya95singh@gmail.com>
2022-05-29 05:14:26 +0530

    first Git commit with java project

---
+++ src/Array/subarray_size_window.java
@@ -0,0 +1,62 @@
+package Array;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class subarray_size_window {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int A[] = new int[n];
+		for( int i=0; i< n; i++){
+			A[i] = sc.nextInt();
+		}
+		int k = sc.nextInt();
+		//for(int i=0; i<A.length; i++)
+		//	System.out.print(A[i]+" ");
+		//System.out.println(k);
+		ArrayList<Integer> count = size_of_window(A , k);
+		System.out.println(count);
+	}
+	public static ArrayList<Integer> size_of_window(int[] A, int k){
+		//System.out.println(" size of window ");
+		int n = A.length;
+		ArrayList<Integer> res = new ArrayList<>();
+		int left = 0 , right = k;
+		HashMap<Integer, Integer> hm = new HashMap<>();
+		for(int i=0; i<k; i++){
+			if(hm.containsKey(A[i])){
+				hm.put(A[i] , hm.get(A[i])+1);
+			}
+			else{
+				hm.put(A[i] , 1);
+			}
+		}
+		//System.out.println(hm);
+		int count = hm.size();
+		res.add(count);
+		//System.out.println(" updated res "+res);
+		while(right<n){
+			if(hm.containsKey(A[left])) {
+				hm.put(A[left], hm.get(A[left]) - 1);
+				if(hm.get(A[left]) == 0)
+					hm.remove(A[left]);
+			}
+			//System.out.println(" l "+hm);
+			if(hm.containsKey(A[right])){
+				hm.put(A[right] , hm.get(A[right])+1);
+			}
+			else{
+				hm.put(A[right] , 1);
+			}
+			res.add(hm.size());
+			left++;
+			right++;
+			//System.out.println(" r "+hm);
+			//System.out.println(" res "+res);
+		}
+		//res.add(count);
+		return res;
+	}
+}
