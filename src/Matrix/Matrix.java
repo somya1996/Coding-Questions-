a952ce65b7727d24c3a73a7b63c120ae4136bb33
Somya <Somya95singh@gmail.com>
2022-05-29 20:30:12 +0530

    first Git commit with java project

--- src/Matrix/Matrix.java
+++ src/Matrix/Matrix.java
@@ -1,21 +1,35 @@
 package Matrix;
 
 import java.util.Scanner;
+/*
+n = 2
+1 2 3 4 5 6 7 8
+9 10 11 12 13 14 15 16
+17 18 19 20 21 22 23 24
+25 26 27 28 29 30 31 32
+33 34 35 36 37 38 39 40
+41 42 43 44 45 46 47 48
+49 50 51 52 53 54 55 56
+57 58 59 60 61 62 63 64
 
+
+36 28 20 21 22 30 38 46 54 53 52 51 50 42 34 26 18 10 2 3 4 5 6 7 8 16 24 32 40 48 56 64
+29 37 45 44 43 35 27 19 11 12 13 14 15 23 31 39 47 55 63 62 61 60 59 58 57 49 41 33 25 17 9 1
+
+*/
 public class Matrix {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		formCoils(n);
-		//int ans[][] = formCoils(n);
-		//int count = 0;
-		//for(int i=0; i<4*n; i++){
-		//	for(int j = 0; j<4*n; j++){
-		//		System.out.println(ans[i][j]);
-		//	}
-		//}
+		int res[][] = formCoils(n);
+		for(int i=0; i< res.length; i++){
+			for(int j = res[0].length-1; j>=0; j--){
+				System.out.print(res[i][j]+" ");
+			}
+			System.out.println();
+		}
 	}
-	static void formCoils(int n) {
+	static int[][] formCoils(int n) {
 		// code here
 		int ans[][] = new int[4*n][4*n];
 		int count = 1;
@@ -24,13 +38,13 @@ public class Matrix {
 				ans[i][j] = count++;
 			}
 		}
-		for(int i=0; i<4*n; i++) {
+		/*for(int i=0; i<4*n; i++) {
 			for (int j = 0; j < 4 * n; j++) {
 				System.out.print(ans[i][j] + " ");
 			}
 			System.out.println();
 		}
-		System.out.println();
+		System.out.println();*/
 
 		int total_size = 4*n*4*n;
 		int res[][] = new int[2][total_size/2];
@@ -112,14 +126,14 @@ public class Matrix {
 			}
 
 		}
-		System.out.println();
-		for(int i=0; i< res.length; i++){
+		//System.out.println();
+		/*for(int i=0; i< res.length; i++){
 			for(int j = res[0].length-1; j>=0; j--){
 				System.out.print(res[i][j]+" ");
 			}
 			System.out.println();
-		}
-		//return ans;
+		}*/
+		return res;
 
 	}
 }
