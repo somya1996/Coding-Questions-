3e07e50879382c045f1a5da4a5e20ac4b7d93bb6
Somya <Somya95singh@gmail.com>
2022-06-08 18:22:30 +0530

    first Git commit with java project

---
+++ src/Array/medium_level/Minimum_platforms.java
@@ -0,0 +1,39 @@
+package Array.medium_level;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Minimum_platforms {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int arrival[] = new int[n];
+		for(int i=0; i<n; i++){
+			arrival[i] = sc.nextInt();
+		}
+		int departure[] = new int[n];
+		for(int i=0; i<n; i++){
+			departure[i] = sc.nextInt();
+		}
+		int platform = findPlatform(arrival , departure , n);
+		System.out.println(platform);
+	}
+	public static int findPlatform(int arr[], int dep[], int n){
+		Arrays.sort(arr);
+		Arrays.sort(dep);
+		int req_platform = 1;
+		int i=1 , j =0;
+		while(i<n && j<n){
+			System.out.println("arr[i] "+arr[i]+" dep[j] "+dep[j]);
+			if(dep[j] > arr[i]){
+				req_platform++;
+				System.out.println(req_platform);
+			}
+			else{
+				j++;
+			}
+			i++;
+		}
+		return req_platform;
+	}
+}
