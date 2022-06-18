5d506ea0e57f24ecfcd2462516c8698fe370b39d
Somya <Somya95singh@gmail.com>
2022-06-18 22:34:38 +0530

    first Git commit with java project

--- src/Array/Array_of_prime_no.java
+++ src/Array/Array_of_prime_no.java
@@ -25,4 +25,31 @@ public class Array_of_prime_no {
 		}
 		return isprime;
 	}
+
+	public static class Freq_Lmt_Range_Arr_Ele {
+		public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			int  n = sc.nextInt();
+			int arr[] = new int[n];
+			for(int i=0; i<n; i++){
+				arr[i] = sc.nextInt();
+			}
+			int p = sc.nextInt();
+			frequencyCount(arr,n,p);
+		}
+		public static void frequencyCount(int arr[], int N, int P){
+			int hash[] = new int[N];
+			Arrays.fill(hash, 0);
+			int i = 0;
+			while (i < N){
+				hash[arr[i] - 1]++;
+				i++;
+			}
+			for(i = 0; i < N; i++)
+			{
+				System.out.println((i + 1) + " -> " +
+						hash[i]);
+			}
+		}
+	}
 }
