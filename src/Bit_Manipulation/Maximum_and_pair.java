01430c760d20221741d9e234780e86093881b1ef
Somya <Somya95singh@gmail.com>
2022-06-20 12:17:21 +0530

    first Git commit with java project

---
+++ src/Bit_Manipulation/Maximum_and_pair.java
@@ -0,0 +1,34 @@
+package Bit_Manipulation;
+
+import java.util.Scanner;
+
+public class Maximum_and_pair {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+		for(int i=0; i<n; i++){
+			arr[i] = sc.nextInt();
+		}
+		int max = maxAND(arr , n);
+		System.out.println(max);
+	}
+	public static int maxAND (int arr[], int N) {
+		int count=0 , res = 0;
+		for(int bit = 31; bit >= 0; bit--){
+			 count = countBit(res|(1<<bit) , arr , N);
+			 if(count >= 2){
+				 res = res|(1<<bit);
+			 }
+		}
+		return res;
+	}
+	public static int countBit(int pattern , int[] arr , int N){
+		int count = 0;
+		for(int i=0; i<N; i++) {
+			if ((pattern & arr[i]) == pattern)
+				count++;
+		}
+		return count;
+	}
+}
