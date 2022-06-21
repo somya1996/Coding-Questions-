603693cfec39ea0d0c628befae771b4fd214fe0f
Somya <Somya95singh@gmail.com>
2022-06-21 17:59:34 +0530

    first Git commit with java project

---
+++ src/RECURSION/SumOfNnumber.java
@@ -0,0 +1,31 @@
+package RECURSION;
+
+import java.util.Scanner;
+
+public class SumOfNnumber {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		//int sum = summing(n);
+		//System.out.println(sum);
+
+		summing(n,0);
+	}
+	// using functional recursion
+	// TC = O(n)
+	// SC = O(n)
+	/*(public static int summing(int n){
+		if(n == 0)
+			return 0;
+		return n + summing(n-1);
+	}*/
+
+	// using parameterized function
+	public static int summing(int i , int sum){
+		if(i<1) {
+			System.out.println(sum);
+			return 0;
+		}
+		return summing(i-1 , sum +i);
+	}
+}
