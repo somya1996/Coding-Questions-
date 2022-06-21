d8d49e89767a722ab0b60f49cb2529d7e85491aa
Somya <Somya95singh@gmail.com>
2022-06-21 17:38:45 +0530

    first Git commit with java project

--- src/RECURSION/Print1toN$Backtracking.java
+++ src/RECURSION/Print1toN$Backtracking.java
@@ -1,4 +1,18 @@
 package RECURSION;
 
+import java.util.Scanner;
+
 public class Print1toN$Backtracking {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		calling1toN(n);
+	}
+	public static int calling1toN(int n){
+		if(n == 0)
+			return 0;
+		calling1toN(n-1);
+		System.out.print(n +" ");
+		return 0;
+	}
 }
