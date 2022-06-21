1dc741bbf93e4c40de2231de2790cfdd53afa942
Somya <Somya95singh@gmail.com>
2022-06-21 18:53:10 +0530

    first Git commit with java project

---
+++ src/RECURSION/Reverse_an_array.java
@@ -0,0 +1,44 @@
+package RECURSION;
+
+import java.util.Scanner;
+// using parameterized and functional recursion
+public class Reverse_an_array {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+		for(int i=0; i<n; i++){
+			arr[i] = sc.nextInt();
+		}
+		// parameterized
+		//reverse(0 , arr , n-1);
+
+		// functional
+		reverse(0 , arr);
+		for(int i=0; i<n; i++){
+			System.out.print(arr[i]+" ");
+		}
+	}
+	// using parameterized recursion
+	/*public static void reverse(int l , int[] arr ,  int r){
+		if(l>=r)
+			return ;
+		int temp = arr[l];
+		arr[l] = arr[r];
+		arr[r] = temp;
+		reverse(l+1 , arr , r-1);
+	}*/
+	// using functional recursion
+	public static void reverse(int i , int[] arr){
+		int n = arr.length;
+		if(i >= n/2)
+			return;
+		swap(i , n-i-1 , arr);
+		reverse(i+1 , arr);
+	}
+	public static void swap(int i , int j , int[] arr){
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+}
