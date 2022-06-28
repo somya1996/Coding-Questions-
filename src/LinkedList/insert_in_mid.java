7e14cf47013825c67532cbb3aa7f8917f72199db
Somya <Somya95singh@gmail.com>
2022-06-28 22:51:36 +0530

    first Git commit with java project

---
+++ src/LinkedList/insert_in_mid.java
@@ -0,0 +1,64 @@
+package LinkedList;
+
+public class insert_in_mid {
+	static Node head;
+	static class Node{
+		int data;
+		Node next;
+		Node(int x){
+			data = x;
+			next = null;
+		}
+	}
+	public static void insertAtMid(int x) {
+		if (head == null)
+			head = new Node(x);
+		else {
+			Node newNode = new Node(x);
+			Node ptr = head;
+			int len = 0;
+
+			while (ptr != null) {
+				len++;
+				ptr = ptr.next;
+			}
+
+			int count = ((len % 2) == 0) ? (len / 2) :
+					(len + 1) / 2;
+			ptr = head;
+
+			while (count-- > 1)
+				ptr = ptr.next;
+
+			newNode.next = ptr.next;
+			ptr.next = newNode;
+		}
+	}
+	static void display()
+	{
+		Node temp = head;
+		while (temp != null)
+		{
+			System.out.print(temp.data + " ");
+			temp = temp.next;
+		}
+	}
+	public static void main(String[] args) {
+		head = null;
+		head = new Node(1);
+		head.next = new Node(2);
+		head.next.next = new Node(4);
+		head.next.next.next = new Node(5);
+
+		System.out.println("Linked list before "+
+				"insertion: ");
+		display();
+
+		int x = 3;
+		insertAtMid(x);
+
+		System.out.println("\nLinked list after"+
+				" insertion: ");
+		display();
+	}
+}
