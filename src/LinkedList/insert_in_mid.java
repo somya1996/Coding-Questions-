b321017b7f0d5a0a6ee7495ff77a997691859958
Somya <Somya95singh@gmail.com>
2022-06-29 05:52:13 +0530

    first Git commit with java project

--- src/LinkedList/insert_in_mid.java
+++ src/LinkedList/insert_in_mid.java
@@ -10,29 +10,26 @@ public class insert_in_mid {
 			next = null;
 		}
 	}
-	public static void insertAtMid(int x) {
+	public static void insertAtMid(Node head, int data){
 		if (head == null)
-			head = new Node(x);
-		else {
-			Node newNode = new Node(x);
-			Node ptr = head;
-			int len = 0;
+			head = new Node(data);
 
-			while (ptr != null) {
-				len++;
-				ptr = ptr.next;
-			}
-
-			int count = ((len % 2) == 0) ? (len / 2) :
-					(len + 1) / 2;
-			ptr = head;
-
-			while (count-- > 1)
-				ptr = ptr.next;
-
-			newNode.next = ptr.next;
-			ptr.next = newNode;
+		Node newNode = new Node(data);
+		Node temp = head;
+		int len = 0;
+		while (temp != null) {
+			len++;
+			temp = temp.next;
 		}
+
+		int count = ((len % 2) == 0) ? (len / 2) : (len + 1) / 2;
+		temp = head;
+
+		while (count-- > 1)
+			temp = temp.next;
+
+		newNode.next = temp.next;
+		temp.next = newNode;
 	}
 	static void display()
 	{
@@ -50,15 +47,13 @@ public class insert_in_mid {
 		head.next.next = new Node(4);
 		head.next.next.next = new Node(5);
 
-		System.out.println("Linked list before "+
-				"insertion: ");
+		System.out.println("Linked list before "+ "insertion: ");
 		display();
 
 		int x = 3;
-		insertAtMid(x);
+		insertAtMid(head , x);
 
-		System.out.println("\nLinked list after"+
-				" insertion: ");
+		System.out.println("\nLinked list after"+ " insertion: ");
 		display();
 	}
 }
