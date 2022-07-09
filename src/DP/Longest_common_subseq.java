0c28fcab5ad7920fecfb9e82e9e91605bbb807e0
Somya <Somya95singh@gmail.com>
2022-07-09 17:38:37 +0530

    first Git commit with java project

--- src/DP/Longest_common_subseq.java
+++ src/DP/Longest_common_subseq.java
@@ -20,9 +20,12 @@ public class Longest_common_subseq {
 
 		int len2 = LCSubseq_memo(s1, s2, n, m, dp);
 		System.out.println(len2);
+
+		int len3 = tabulation(s1, s2, n, m);
+		System.out.println(len3);
 	}
 
-	// using memoization
+	// using memoization (bottom to top)
 
 	public static int LCSubseq_memo(String S1, String S2, int n, int m, int[][] dp) {
 		//base case
@@ -41,5 +44,29 @@ public class Longest_common_subseq {
 			return dp[n][m];
 		}
 
+	}
+	// using tabulation (top - down approach)
+	public static int tabulation(String s1 , String s2 , int n  , int m ){
+		int dp[][] = new int[n+1][m+1]; // start from length of string as 0 to length of string
+		// base case -> first row and col are 0 because if there is no string then lcs = 0
+		for(int i= 0 ; i<n+1; i++){
+			for(int j = 0; j<m+1; j++){
+				if(i == 0 || j == 0)
+					dp[i][j] = 0;
+			}
+		}
+		// choice diagram
+		// i and j are already filled
+		for(int i= 1; i<n+1; i++){
+			for(int j = 1; j<m+1; j++){
+				if(s1.charAt(i-1) == s2.charAt(j-1))
+					dp[i][j] = 1 + dp[i-1][j-1];
+
+				else{
+					dp[i][j] = Math.max(dp[i-1][j] , dp[i][j-1]);
+				}
+			}
+		}
+		return dp[n][m];
 	}
 }
