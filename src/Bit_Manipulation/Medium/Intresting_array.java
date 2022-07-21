07284ee8caf1c65e96208cb47b5639c1b8db586f
Somya <Somya95singh@gmail.com>
2022-07-21 09:52:00 +0530

    first Git commit with java project

--- src/Bit_Manipulation/Medium/Intresting_array.java
+++ src/Bit_Manipulation/Medium/Intresting_array.java
@@ -16,7 +16,9 @@ public class Intresting_array {
 		System.out.println(s);
 	}
 	public static String solve(int[] A) {
-		HashSet<Integer> hs = new HashSet<>();
+		// Time Complexity - O(n)
+		// Space Complexity - O(n)
+		/*HashSet<Integer> hs = new HashSet<>();
 		int xor = 0;
 		for(int i=0; i<A.length; i++){
 			xor ^= A[i];
@@ -39,6 +41,31 @@ public class Intresting_array {
 			//System.out.println(" b "+b);
 			xor = a^b;
 			hs.add(xor);
+		}*/
+		/*
+		1 > If any element in the array is even then, it can be made 0.
+		    Split that element into two equal parts of A[i]/2 and A[i]/2.
+		    XOR of two identical numbers is zero. (Therefore this strategy makes an element 0.)
+
+		2 > If any element is odd.
+			Split it in two-part: 1, A[i]-1.
+			Since A[i]-1 is even, it can be made 0 by the above strategy.
+			(Therefore an odd element can reduce its size to 1).
+
+		3 > Two odd elements can be made 0 by following the above strategy (1)
+		    1 XOR 1 = 0.
+
+		4 > Therefore if the number of odd elements in the array is even, the answer is possible.
+			Otherwise, an element of value 1 will be left, and it is impossible to satisfy the condition.
+		 */
+		int cnt = 0;
+		for (int x: A) {
+			if (x % 2 == 1)
+				cnt++;
 		}
+		if (cnt % 2 == 1)
+			return "No";
+		else
+			return "Yes";
 	}
 }
