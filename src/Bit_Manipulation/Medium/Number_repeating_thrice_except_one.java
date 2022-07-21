c390b4dc8a5bdcfcee5a708e7bbb1b5bb39f9843
Somya <Somya95singh@gmail.com>
2022-07-21 06:21:54 +0530

    first Git commit with java project

---
+++ src/Bit_Manipulation/Medium/Number_repeating_thrice_except_one.java
@@ -0,0 +1,44 @@
+package Bit_Manipulation.Medium;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Number_repeating_thrice_except_one {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+		for(int i=0; i<n; i++){
+			arr[i] = sc.nextInt();
+		}
+		int ans = single_no_in_thrice(arr);
+		System.out.println("ans "+ans);
+	}
+	public static int single_no_in_thrice(int[] A){
+		int[] freq = new int[32];
+
+		for(int x: A){
+			int k = 0;
+			int p = x;
+			while(p !=0) {
+				int last_Bit = (p & 1);
+				if (last_Bit == 1) {
+					freq[k]++;
+				}
+				k++;
+				p = p>>1;
+			}
+			//x = x >> 1;
+		}
+		for(int i=0; i<freq.length; i++){
+			System.out.print(freq[i]+" ");
+		}
+		int ans = 0;
+		int mul = 1;
+		for(int i=0; i<32; i++){
+			ans += freq[i]%3 * mul;
+			mul = mul<<1;
+		}
+		return ans;
+	}
+}
