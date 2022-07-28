5f4efd41d68e84602eeea4eb274523574c803a37
Somya <Somya95singh@gmail.com>
2022-07-28 08:18:18 +0530

    first Git commit with java project

--- src/Array/Hard_level/Count_pair_sum_divisible_by_m.java
+++ src/Array/Hard_level/Count_pair_sum_divisible_by_m.java
@@ -16,61 +16,12 @@ public class Count_pair_sum_divisible_by_m {
 		System.out.println(total);
 	}
 	public static int Pair_Sum(int arr[] , int k){
-		/*
-		HashMap<Integer,Integer> hm = new HashMap<>();
-		int count = 0;
-		for(int x : arr){
-			int rem = x%k;
-			if(hm.containsKey(rem)){
-				hm.put(rem , hm.get(rem)+1);
-			}
-			else{
-				hm.put(rem , 1);
-			}
-		}
-		System.out.println(hm);
-		for(int x : hm.keySet()){
-			int rem = x % k;
-			//case 1 :
-			if(rem == 0){
-				int freq = hm.get(rem);
-				if(freq > 2){
-					count += freq/2;
-				}
-			}
-			//case 2 :
-			if(2*rem == k){
-				int freq = hm.get(rem);
-				//if(freq % 2 == 0){
-				//	count += freq;
-				//}
-				if(freq > 2){
-					count += freq/2;
-				}
-			}
-			// case 3:
-			else{
-				int freq = hm.get(rem);  //k
-				System.out.println(" rem "+rem);
-				System.out.println(" freq "+freq);
-				int ofreq = hm.getOrDefault(k-rem , 0); // k-x
-				System.out.println(" ofreq "+ofreq);
-				if(freq == ofreq){
-					count += (freq*ofreq);
-					System.out.println(" count "+count);
-				}
-			}
-		}
-		return count;
-		*/
-
-
-		int freq[] = new int[k];
+		int freq[] = new int[k+1];
 		Arrays.fill(freq , 0);
 		int count = 0;
 		for(int i=0; i<arr.length; i++){
 			int rem = arr[i]%k;
-			int prev_freq = freq[arr[k-rem]%k];
+			int prev_freq = freq[(k-rem)%k];
 			count += prev_freq;
 			freq[rem]++;
 		}
