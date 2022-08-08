e0760101881763f55fc1d9354ea208cd0bf79a57
Somya <Somya95singh@gmail.com>
2022-08-08 23:07:42 +0530

    first Git commit with java project

---
+++ src/Array/Subarray_sum_divisible_by_k.java
@@ -0,0 +1,43 @@
+package Array;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Subarray_sum_divisible_by_k {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+		for(int i=0; i<n; i++){
+			arr[i] = sc.nextInt();
+		}
+		int k = sc.nextInt();
+		int count = solve(arr , k);
+		System.out.println(count);
+	}
+	public static int solve(int[] nums , int k){
+		HashMap<Integer , Integer> hm = new HashMap<>();
+		int sum = 0;
+		int rem = 0;
+		int count = 0;
+		// hashmap<remainder , frequency>
+		hm.put(0 , 1);
+		for(int i=0; i<nums.length; i++){
+			sum +=  nums[i];
+			rem = sum % k;
+
+			if(rem < 0){
+				rem += k;
+			}
+
+			if(hm.containsKey(rem)){
+				count += hm.get(rem);
+				hm.put(rem , hm.get(rem)+1);
+			}
+			else{
+				hm.put(rem , 1);
+			}
+		}
+		return count;
+	}
+}
