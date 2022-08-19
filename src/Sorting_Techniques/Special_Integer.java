32b97b5302f4aa9bd547df73715683049d9b6c63
Somya <Somya95singh@gmail.com>
2022-08-19 14:14:39 +0530

    first Git commit with java project

--- src/Sorting_Techniques/Special_Integer.java
+++ src/Sorting_Techniques/Special_Integer.java
@@ -16,8 +16,8 @@ public class Special_Integer {
 		System.out.println(solve(arr , B));
 	}
 	public static int solve(int[] A, int B) {
-		//TC O(nlogn)
-		int n = A.length;
+		//TC O(nlogn) - linear
+		/*int n = A.length;
 		int sum = 0 , ans = n;
 		int l = 0;
 		for(int i=0; i<n; i++) {
@@ -39,5 +39,37 @@ public class Special_Integer {
 			return 0;
 		//return Math.min(ans , n);
 		return ans;
+		*/
+
+		// using binary search
+		int[] prefix = new int[A.length];
+		int ans = 0;
+		prefix[0] = A[0];
+		for(int i=1; i<A.length; i++){
+			prefix[i] = prefix[i-1]+A[i];
+		}
+
+		int start = 1 , end = A.length;
+		while(start <= end){
+			int mid = (start+end)/2;
+			if(check_mid(A , mid , B)==true){
+				ans = mid;
+				start = mid+1;
+			}
+			else
+				end = mid-1;
+		}
+		return ans;
+	}
+	public static boolean check_mid(int A[] , int mid , int B){
+		for (int i = mid - 1; i < A.length; ++i) {
+			if (i == mid - 1) {
+				if (A[i] > B)
+					return false;
+			} else if (A[i] - A[i - mid] > B) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
