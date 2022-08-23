772932dbe6e3b14001bbe4cffd5596b5c2c3b1ee
Somya <Somya95singh@gmail.com>
2022-08-23 12:34:44 +0530

    first Git commit with java project

---
+++ src/Sorting_Techniques/Medium/To_get_Kth_price_unsorted_array_desc.java
@@ -0,0 +1,45 @@
+package Sorting_Techniques.Medium;
+
+import java.util.Scanner;
+
+public class To_get_Kth_price_unsorted_array_desc {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int price[] = new int[n];
+		for(int i=0; i<n; i++){
+			price[i] = sc.nextInt();
+		}
+		int index = sc.nextInt();
+		System.out.println(solve(price , index));
+	}
+	public static int solve(final int[] A, int B) {
+		int max = Integer.MIN_VALUE , min = Integer.MIN_VALUE;
+		int ans = -1;
+		for(int i=0; i<A.length; i++){
+			max = Math.max(max , A[i]);
+			min = Math.min(min , A[i]);
+		}
+		int start = min , end = max;
+		while(start <= end){
+			int mid = (start + end)/2;
+			int number = count(A , mid);
+			if( number >= B){
+				ans = mid;
+				start = mid+1;
+			}
+			else{
+				end = mid -1;
+			}
+		}
+		return ans;
+	}
+	public static int count(int[] a , int mid){
+		int num = 0;
+		for(int i=0; i< a.length; i++){
+			if(a[i]>= mid)
+				num++;
+		}
+		return num;
+	}
+}
