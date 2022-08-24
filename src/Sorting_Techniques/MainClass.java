dfca60f95f552b24a5d3bba6bf686d8c515bc6f4
Somya <Somya95singh@gmail.com>
2022-08-24 09:22:13 +0530

    first Git commit with java project

--- src/Sorting_Techniques/MainClass.java
+++ src/Sorting_Techniques/MainClass.java
@@ -2,6 +2,7 @@ package Sorting_Techniques;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class MainClass {
@@ -12,8 +13,21 @@ public class MainClass {
 		students.add(new Student_class(83 , "Aman"));
 		students.add(new Student_class(13 , "Rohit"));
 		students.add(new Student_class(13 , "Anuj"));
+		students.add(new Student_class(12 , "Ram"));
 
-		Collections.sort(students);
+
+		Collections.sort(students , new SortByNameThenMarks());
 		students.forEach(System.out::println);
+	}
+}
+class SortByNameThenMarks implements Comparator<Student_class>{
+	@Override
+	public int compare(Student_class s1 , Student_class s2){
+		if(s1.name.equals(s2.name)){
+			return s1.marks - s2.marks;
+		}
+		else{
+			return s1.name.compareTo(s2.name);
+		}
 	}
 }
