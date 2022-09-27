06c831fb20d59c06e93224234869738f209b3662
Somya <Somya95singh@gmail.com>
2022-09-27 21:01:19 +0530

    -

--- src/LinkedList/Medium/Delete_odd_pos_nodes_CLL.java
+++ src/LinkedList/Medium/Delete_odd_pos_nodes_CLL.java
@@ -3,119 +3,296 @@ package LinkedList.Medium;
 import java.util.Scanner;
 
 public class Delete_odd_pos_nodes_CLL {
-	static Node head;
-	static class Node{
-		int val;
+	static class Node
+	{
+		int data;
 		Node next;
-		Node(int x){
-			val = x;
-			next = null;
+	};
+
+	// Function return number of nodes present in list
+	static int Length(Node head)
+	{
+		Node current = head;
+		int count = 0;
+
+		// if list is empty simply return length zero
+		if (head == null)
+		{
+			return 0;
+		}
+
+		// traverse first to last node
+		else
+		{
+			do
+			{
+				current = current.next;
+				count++;
+			} while (current != head);
+		}
+		return count;
+	}
+
+	// Function print data of list
+	static void Display( Node head)
+	{
+		Node current = head;
+
+		// if list is empty simply show message
+		if (head == null)
+		{
+			System.out.printf("\nDisplay List is empty\n");
+			return;
+		}
+
+		// traverse first to last node
+		else
+		{
+			do
+			{
+				System.out.printf("%d ", current.data);
+				current = current.next;
+			} while (current != head);
 		}
 	}
-	public static Node insert(Node head , int data){
+
+	/* Function to insert a node at the end of
+	a Circular linked list */
+	static Node Insert(Node head, int data)
+	{
 		Node current = head;
 		// Create a new node
-		Node newNode = new Node(data);
-		System.out.println(newNode.val);
+		Node newNode = new Node();
+
 		// check node is created or not
-		if (newNode == null) {
+		if (newNode == null)
+		{
 			System.out.printf("\nMemory Error\n");
 			return null;
 		}
-		//newNode.val = data;
-		if (head == null){
+
+		// insert data into newly created node
+		newNode.data = data;
+
+		// check list is empty
+		// if not have any node then
+		// make first node it
+		if (head == null)
+		{
 			newNode.next = newNode;
 			head = newNode;
 			return head;
 		}
+
 		// if list have already some node
 		else
 		{
 			// move first node to last node
-			while (current.next != head) {
+			while (current.next != head)
+			{
 				current = current.next;
 			}
+
 			// put first or head node address in new node link
 			newNode.next = head;
+
 			// put new node address into last node link(next)
 			current.next = newNode;
 		}
 		return head;
 	}
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		for(int i=0; i<n; i++) {
-			int x = sc.nextInt();
-				insert(head , x);
-			}
-		printll(head);
-		deleteOddNode(head);
-		printll(head);
-	}
-	public static void printll(Node head){
-		Node temp = head;
-		while(temp != head) {
-			System.out.print(temp.val+" ");
+
+	// Utility function to delete a Node
+	static Node deleteNode(Node head_ref, Node del)
+	{
+		Node temp = head_ref;
+
+		// If node to be deleted is head node
+		if (head_ref == del)
+		{
+			head_ref = del.next;
+		}
+
+		// traverse list till not found
+		// delete node
+		while (temp.next != del)
+		{
 			temp = temp.next;
 		}
-		System.out.println();
+
+		// copy address of node
+		temp.next = del.next;
+		return head_ref;
 	}
-	public static void deleteOddNode(Node head){
-		int len = size(head);
+
+	// Function to delete First node of
+// Circular Linked List
+	static Node DeleteFirst(Node head)
+	{
+		Node previous = head, next = head;
+		// check list have any node
+		// if not then return
+		if (head == null)
+		{
+			System.out.printf("\nList is empty\n");
+			return head;
+		}
+
+		// check list have single node
+		// if yes then delete it and return
+		if (previous.next == previous)
+		{
+			head = null;
+			return head;
+		}
+
+		// traverse second to first
+		while (previous.next != head)
+		{
+			previous = previous.next;
+			next = previous.next;
+		}
+
+		// now previous is last node and
+		// next is first node of list
+		// first node(next) link address
+		// put in last node(previous) link
+		previous.next = next.next;
+
+		// make second node as head node
+		head = previous.next;
+		return head;
+	}
+
+	// Function to delete odd position nodes
+	static Node DeleteAllOddNode(Node head)
+	{
+		int len = Length(head);
 		int count = 0;
 		Node previous = head, next = head;
-		if (head == null){
-			return;
+
+		// check list have any node
+		// if not then return
+		if (head == null)
+		{
+			System.out.printf("\nDelete Last List is empty\n");
+			return null;
 		}
-		if (len == 1){
-			headdel(head);
-			return;
+
+		// if list have single node means
+		// odd position then delete it
+		if (len == 1)
+		{
+			head = DeleteFirst(head);
+			return head;
 		}
-		while (len > 0){
-			if (count == 0){
-				headdel(head);
+
+		// traverse first to last if
+		// list have more than one node
+		while (len > 0)
+		{
+			// delete first position node
+			// which is odd position
+			if (count == 0)
+			{
+
+				// Function to delete first node
+				head = DeleteFirst(head);
 			}
-			if (count % 2 == 0 && count != 0){
-				delNodes(head, previous);
+
+			// check position is odd or not
+			// if yes then delete node
+			// Note: Considered 1 based indexing
+			if (count % 2 == 0 && count != 0)
+			{
+				head = deleteNode(head, previous);
 			}
+
 			previous = previous.next;
 			next = previous.next;
 
 			len--;
 			count++;
 		}
-		return;
-	}
-	public static int size(Node head){
-		Node temp = head;
-		int count = 0;
-		while(temp != head){
-			count++;
-			temp = temp.next;
-		}
-		return count;
-	}
-	public static Node headdel(Node head){
-		Node t1 = head;
-		Node t2 = t1.next;
-		while(t1.next != head){
-			t1 = t1.next;
-		}
-		t1.next = t2;
-		head = t2;
-		return head;
-	}
-	public static Node delNodes(Node head, Node del){
-		Node temp = head;
-		if (head == del){
-			head = del.next;
-		}
-		while (temp.next != del){
-			temp = temp.next;
-		}
-		temp.next = del.next;
 		return head;
 	}
 
+	// Function to delete all even position nodes
+	static Node DeleteAllEvenNode( Node head)
+	{
+		// Take size of list
+		int len = Length(head);
+
+		int count = 1;
+		Node previous = head, next = head;
+
+		// Check list is empty
+		// if empty simply return
+		if (head == null)
+		{
+			System.out.printf("\nList is empty\n");
+			return null;
+		}
+
+		// if list have single node
+		// then return
+		if (len < 2)
+		{
+			return null;
+		}
+
+		// make first node is previous
+		previous = head;
+
+		// make second node is current
+		next = previous.next;
+
+		while (len > 0)
+		{
+
+			// check node number is even
+			// if node is even then
+			// delete that node
+			if (count % 2 == 0)
+			{
+				previous.next = next.next;
+				previous = next.next;
+				next = previous.next;
+			}
+
+			len--;
+			count++;
+		}
+		return head;
+	}
+
+	// Driver Code
+	public static void main(String args[])
+	{
+		Node head = null;
+		head = Insert(head, 99);
+		head = Insert(head, 11);
+		head = Insert(head, 22);
+		head = Insert(head, 33);
+		head = Insert(head, 44);
+		head = Insert(head, 55);
+		head = Insert(head, 66);
+
+		// Deleting Odd positioned nodes
+		System.out.printf("Initial List: ");
+		Display(head);
+
+		System.out.printf("\nAfter deleting Odd position nodes: ");
+		head = DeleteAllOddNode(head);
+		Display(head);
+
+		// Deleting Even positioned nodes
+		System.out.printf("\n\nInitial List: ");
+		Display(head);
+
+		System.out.printf("\nAfter deleting even position nodes: ");
+		head = DeleteAllEvenNode(head);
+		Display(head);
+
+	}
 }
