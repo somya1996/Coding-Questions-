ce4c81ab27522f6746f07764ed96872066af6e10
Somya <Somya95singh@gmail.com>
2022-10-15 11:04:44 +0530

    -

--- src/BInaryTree/Find_closest_to_target_in_BST.java
+++ src/BInaryTree/Find_closest_to_target_in_BST.java
@@ -1,7 +1,5 @@
 package BInaryTree;
 
-import groovyjarjarantlr4.runtime.tree.Tree;
-
 public class Find_closest_to_target_in_BST {
 	static TreeNode root;
 	static class TreeNode{
@@ -14,6 +12,8 @@ public class Find_closest_to_target_in_BST {
 			right = null;
 		}
 	}
+	static int mindif = Integer.MAX_VALUE;
+	static int value = 0;
 	public static void main(String[] args) {
 		// create root
 		root = new TreeNode(10);
@@ -29,10 +29,13 @@ public class Find_closest_to_target_in_BST {
 		root.right.right.right = new TreeNode(22);
 
 		int target = 17;
-		int ans = nearest_target(root , target);
+		int ans = nearest_target_iteratively(root , target);
+		System.out.println(ans);
+		ans = nearest_target_recursively(root , target);
 		System.out.println(ans);
 	}
-	public static int nearest_target(TreeNode root , int target){
+	//iterative
+	public static int nearest_target_iteratively(TreeNode root , int target){
 		TreeNode temp = root;
 		int ans = temp.data;
 		int min_diff = Integer.MAX_VALUE;
@@ -56,5 +59,23 @@ public class Find_closest_to_target_in_BST {
 		}
 		return ans;
 
+	}
+	public static int nearest_target_recursively(TreeNode root  , int target){
+		// base case
+		if(root == null)
+			return 0;
+		// rec case
+		int d = Math.abs(root.data - target);
+		if(d < mindif){
+			mindif = d;
+			value = root.data;
+		}
+
+		if(root.data > target && root.left != null)
+			return nearest_target_recursively(root.left , target);
+		else if(root.data < target && root.right != null)
+			return nearest_target_recursively(root.right , target);
+		else
+			return value;
 	}
 }
