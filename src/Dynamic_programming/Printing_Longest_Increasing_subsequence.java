d4768e6f9aa0915191d3492ed3d90f261a391e07
Somya <Somya95singh@gmail.com>
2022-11-20 17:08:05 +0530

    -

---
+++ src/Dynamic_programming/Printing_Longest_Increasing_subsequence.java
@@ -0,0 +1,59 @@
+package Dynamic_programming;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Printing_Longest_Increasing_subsequence {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i=0; i<n; i++){
+			arr[i] = sc.nextInt();
+		}
+		ArrayList<Integer> temp = printing_LIS(arr);
+		for(int i=temp.size()-1; i>=0; i--){
+			System.out.print(temp.get(i)+" ");
+		}
+	}
+	public static ArrayList<Integer> printing_LIS(int[] arr) {
+		int n = arr.length;
+		int[] dp=new int[n];
+		Arrays.fill(dp,1);
+		int[] hash=new int[n];
+		Arrays.fill(hash,1);
+
+		for(int i=0; i<=n-1; i++){
+
+			hash[i] = i; // initializing with current index
+			for(int prev_index = 0; prev_index <=i-1; prev_index ++){
+
+				if(arr[prev_index]<arr[i] && 1 + dp[prev_index] > dp[i]){
+					dp[i] = 1 + dp[prev_index];
+					hash[i] = prev_index;
+				}
+			}
+		}
+
+		int ans = -1;
+		int lastIndex =-1;
+
+		for(int i=0; i<=n-1; i++){
+			if(dp[i]> ans){
+				ans = dp[i];
+				lastIndex = i;
+			}
+		}
+
+		ArrayList<Integer> temp=new ArrayList<>();
+		temp.add(arr[lastIndex]);
+
+		while(hash[lastIndex] != lastIndex){ // till not reach the initialization value
+			lastIndex = hash[lastIndex];
+			temp.add(arr[lastIndex]);
+		}
+
+		return temp;
+	}
+}
